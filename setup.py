4320d2939309ff843acd05675dcabdf42f23cd39
Dominic Steinhöfel <dominic.steinhoefel@cispa.de>
2021-08-03 15:45:34 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='isla',
+    version='0.1',
+    packages=['input_constraints', 'input_constraints.tests'],
+    url='https://projects.cispa.saarland/c01dost/inputconstraints',
+    license='GNU GPLv3',
+    author='Dominic Steinhoefel',
+    author_email='dominic.steinhoefel@cispa.de',
+    description='ISLa (Input Specification Language)'
+)
