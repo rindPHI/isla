b6a119431e4bfa8ed0c87a57b4b20ab3744b37aa
Dominic Steinhöfel <dominic.steinhoefel@cispa.de>
2021-10-19 12:49:46 +0200

    Fixed / removed outdated test cases

--- input_constraints/tests/test_existential_helpers.py
+++ input_constraints/tests/test_existential_helpers.py
@@ -4,14 +4,11 @@ from fuzzingbook.Grammars import JSON_GRAMMAR
 from fuzzingbook.Parser import canonical
 from grammar_graph.gg import GrammarGraph
 
-from input_constraints import isla
 from input_constraints.existential_helpers import insert_tree, wrap_in_tree_starting_in
 from input_constraints.isla import DerivationTree
 from input_constraints.tests.subject_languages import tinyc
-import input_constraints.isla_shortcuts as sc
 from input_constraints.tests.test_data import *
 from input_constraints.tests.test_helpers import parse
-from input_constraints.tests.subject_languages.tinyc import TINYC_GRAMMAR
 
 
 class TestExistentialHelpers(unittest.TestCase):
@@ -57,133 +54,36 @@ class TestExistentialHelpers(unittest.TestCase):
             [result.to_string() for result in results])
 
     def test_insert_assignment(self):
-        assgn = isla.Constant("$assgn", "<assgn>")
+        assgn = DerivationTree.from_parse_tree(("<assgn>", None))
         tree = ('<start>', [('<stmt>', [('<assgn>', [('<var>', None), (' := ', []), ('<rhs>', [('<var>', None)])])])])
         results = insert_tree(
             canonical(LANG_GRAMMAR),
-            DerivationTree(assgn),
+            assgn,
             DerivationTree.from_parse_tree(tree))
 
         self.assertEqual(
-            ['<var> := <var> ; $assgn',
-             '<var> := <var> ; $assgn ; <stmt>',
-             '$assgn ; <var> := <var>',
-             '<assgn> ; $assgn ; <var> := <var>',
+            ['<var> := <var> ; <assgn>',
+             '<var> := <var> ; <assgn> ; <stmt>',
+             '<assgn> ; <var> := <var>',
+             '<assgn> ; <assgn> ; <var> := <var>',
              ],
             list(map(str, results))
         )
 
     def test_insert_assignment_2(self):
-        lhs = isla.Constant("$lhs", "<var>")
-        var = isla.Constant("$var", "<var>")
-
-        tree = ('<start>', [('<stmt>', [('<assgn>', [(lhs, None), (' := ', []), ('<rhs>', [(var, None)])])])])
+        tree = ('<start>', [('<stmt>', [('<assgn>', [("<var>", None), (' := ', []), ('<rhs>', [("<var>", None)])])])])
 
         results = insert_tree(canonical(LANG_GRAMMAR),
                               DerivationTree("<assgn>", None),
                               DerivationTree.from_parse_tree(tree))
 
         self.assertEqual(
-            ['$lhs := $var ; <assgn>',
-             '$lhs := $var ; <assgn> ; <stmt>',
-             '<assgn> ; $lhs := $var',
-             '<assgn> ; <assgn> ; $lhs := $var'],
+            ['<var> := <var> ; <assgn>',
+             '<var> := <var> ; <assgn> ; <stmt>',
+             '<assgn> ; <var> := <var>',
+             '<assgn> ; <assgn> ; <var> := <var>'],
             list(map(str, results))
         )
-
-    def test_insert_tinyc_assignment(self):
-        tree = DerivationTree.from_parse_tree(('<start>', [
-            ('<mwss>', None),
-            ('<statements>', [
-                ('<statement>', [
-                    ('<mwss>', None),
-                    ('<expr>', [('<test>', [('<sum>', [('<term>', [('<id>', None)])])])]),
-                    ('<mwss>', None),
-                    (';', [])]),
-                ('<statement>', [
-                    ('<mwss>', None),
-                    ('<expr>', [
-                        ('<id>', None), ('<mwss>', None), ('=', []), ('<mwss>', None),
-                        ('<expr>', [
-                            ('<id>', None), ('<mwss>', None), ('=', []), ('<mwss>', None),
-                            ('<expr>', [
-                                ('<id>', None), ('<mwss>', None), ('=', []), ('<mwss>', None),
-                                ('<expr>', [
-                                    ('<id>', None), ('<mwss>', None), ('=', []), ('<mwss>', None),
-                                    ('<expr>', None)])])])]),
-                    ('<mwss>', None),
-                    (';', [])])]),
-            ('<mwss>', None)]))
-
-        to_insert = DerivationTree.from_parse_tree(
-            ('<expr>', [('<id>', None), ('<mwss>', None), ('=', []), ('<mwss>', None), ('<expr>', None)]))
-
-        wrapped = wrap_in_tree_starting_in(
-            "<expr>", to_insert, canonical(TINYC_GRAMMAR), GrammarGraph.from_grammar(TINYC_GRAMMAR))
-
-        self.assertTrue(wrapped.find_node(to_insert),
-                        f"{to_insert} not found in result of wrapping in '<expr>': {wrapped}")
-
-        results = insert_tree(canonical(TINYC_GRAMMAR), to_insert, DerivationTree.from_parse_tree(tree))
-
-        for result in results:
-            self.assertTrue(result.find_node(to_insert))
-
-    def test_insert_assignment_tinyc(self):
-        mgr = isla.VariableManager(tinyc.TINYC_GRAMMAR)
-        tree = DerivationTree.from_parse_tree(('<start>', [
-            ('<mwss>', None), ('<statement>', [
-                ('{', []), ('<mwss>', None), ('<statements>', [
-                    ('<statement>', [
-                        ('{', []), ('<mwss>', None), ('<statements>', [
-                            ('<statement>', [
-                                ('{', []), ('<mwss>', None), ('<statements>', [
-                                    ('<statement>', [
-                                        ('<mwss>', None), ('<expr>', [
-                                            ('<test>', [
-                                                ('<sum>', [
-                                                    ('<sum>', [
-                                                        ('<sum>', [
-                                                            ('<sum>', [
-                                                                ('<term>', None)]),
-                                                            ('<mwss>', None), ('-', []), ('<mwss>', None),
-                                                            ('<term>', [('<id>', None)])]),
-                                                        ('<mwss>', None), ('-', []), ('<mwss>', None),
-                                                        ('<term>', [('<int>', None)])]),
-                                                    ('<mwss>', None), ('+', []), ('<mwss>', None),
-                                                    ('<term>', [
-                                                        ('<paren_expr>', [
-                                                            ('(', []), ('<mwss>', None), ('<expr>', [('<test>', None)]),
-                                                            ('<mwss>', None), (')', [])])])]),
-                                                ('<mwss>', None), ('<', []), ('<mwss>', None),
-                                                ('<sum>', [
-                                                    ('<sum>', [
-                                                        ('<term>', [
-                                                            ('<paren_expr>', [
-                                                                ('(', []), ('<mwss>', None), ('<expr>', None),
-                                                                ('<mwss>', None), (')', [])])])]),
-                                                    ('<mwss>', None), ('-', []), ('<mwss>', None),
-                                                    ('<term>', [
-                                                        ('<int>', None)])])])]),
-                                        ('<mwss>', None), (';', [])])]), ('<mwss>', None), ('}', [])])]),
-                        ('<mwss>', None), ('}', [])]), ('<mwss>', None),
-                    ('<statements>', [('<statement>', [(';', [])])])]),
-                ('<mwss>', None), ('}', [])]), ('<mwss>', None)]))
-
-        subtree = tree.get_subtree((1, 2, 0, 2, 0, 2, 0, 1, 0))
-
-        formula = sc.exists_bind(
-            mgr.bv("$id_2", "<id>") + "<mwss>=<mwss><expr>", mgr.bv("$expr", "<expr>"), tree,
-            sc.before(mgr.bv("$expr"), subtree)
-        )
-
-        inserted_tree, bind_expr_paths = formula.bind_expression.to_tree_prefix(
-            formula.bound_variable.n_type, tinyc.TINYC_GRAMMAR)
-
-        insertion_result = insert_tree(canonical(tinyc.TINYC_GRAMMAR), inserted_tree, tree)
-
-        for result_tree in insertion_result:
-            self.assertTrue(result_tree.find_node(inserted_tree))
 
     def test_wrap_tinyc_assignment(self):
         tree = DerivationTree.from_parse_tree(
