ea8cd080540aede3bb7dc3ad5d9a50d4a06153a0
Dominic Steinhöfel <dominic.steinhoefel@cispa.de>
2021-11-17 18:57:49 +0100

    Added python file with code to resume at a dumped solver state

---
+++ resume_solver.py
@@ -0,0 +1,16 @@
+import pickle
+
+from input_constraints.solver import ISLaSolver
+
+with (open("/tmp/saved_debug_state", "rb")) as debug_state_file:
+    try:
+        solver: ISLaSolver = pickle.load(debug_state_file)
+        it = solver.solve()
+        while True:
+            try:
+                result = next(it)
+                print(f"Found solution: {result}")
+            except StopIteration:
+                pass
+    except EOFError as e:
+        print(e)
