05ccf2526efeb5d1b85fe12224fb452d0bff603b
Dominic Steinhöfel <dominic.steinhoefel@cispa.de>
2022-08-25 15:39:48 +0200

    Added parameters to run scripts

--- run_eval_scriptsize_c.fish
+++ run_eval_scriptsize_c.fish
@@ -1,4 +1,18 @@
 #!/usr/bin/env fish
+
+set script (basename (status -f))
+argparse --name="$script" 's/seconds=' 'r/runs=' -- $argv or return
+
+set secs 3600
+if set -q _flag_s
+  set secs $_flag_s
+end
+
+set runs 2
+if set -q _flag_r
+  set runs $_flag_r
+end
+
 if test -z "$VIRTUAL_ENV" -a -d venv
   source venv/bin/activate.fish
 end
@@ -12,8 +26,8 @@ set db "$curr_dir/isla_evaluation_scriptsize_c.sqlite"
 # Scriptsize-C
 set jobs "Grammar Fuzzer" "Def-Use" "No-Redef" "Def-Use + No-Redef"
 for j in $jobs
-  for n in (seq 2)
-    python3 -u -O evaluations/evaluate_scriptsize_c.py -g -t 3600 -j "$j" --db "$db"
+  for n in (seq $runs)
+    python3 -u -O evaluations/evaluate_scriptsize_c.py -g -t $secs -j "$j" --db "$db"
   end
 end
 
