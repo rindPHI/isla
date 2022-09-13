4eeaf43b231a16c3afcf69d87fd62317308e5e71
Dominic Steinhöfel <dominic.steinhoefel@cispa.de>
2022-09-13 16:12:01 +0200

    Solver parser mostly finished (with exception of input processing)

--- src/isla/cli.py
+++ src/isla/cli.py
@@ -1,5 +1,206 @@
+import argparse
+import sys
+
+
 def main():
-    print("Hello!")
+    parser = create_parsers()
+
+    args = parser.parse_args()
+    args.func(args)
+
+
+def create_parsers():
+    parser = argparse.ArgumentParser(
+        prog="isla",
+        description="""
+The ISLa command line interface.""",
+    )
+    subparsers = parser.add_subparsers(title="Commands", required=True)
+    create_solve_parser(subparsers)
+    create_fuzz_parser(subparsers)
+    create_check_parser(subparsers)
+    create_parse_parser(subparsers)
+
+    return parser
+
+
+def solve(parser, args):
+    files = {io_wrapper.name: io_wrapper.read() for io_wrapper in args.files}
+
+    if all(not file.endswith(".bnf") for file in files):
+        parser.print_usage(file=sys.stderr)
+        print(
+            "isla solve: error: You must specify a grammar by `--grammar` "
+            "or FILES arguments with `.bnf` ending.",
+            file=sys.stderr,
+        )
+
+        exit(2)
+
+    if all(not file.endswith(".isla") for file in files):
+        parser.print_usage(file=sys.stderr)
+        print(
+            "isla solve: error: You must specify a constraint by `--constraint` "
+            "or FILES arguments with `.isla` ending.",
+            file=sys.stderr,
+        )
+
+        exit(2)
+
+
+def fuzz(parser, args):
+    print(args)
+
+
+def check(parser, args):
+    print(args)
+
+
+def parse(parser, args):
+    print(args)
+
+
+def create_solve_parser(subparsers):
+    parser = subparsers.add_parser(
+        "solve",
+        help="create solutions to ISLa constraints or check their unsatisfiability",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description="""
+Create solutions to an ISLa constraint and a reference grammar.""",
+    )
+    parser.set_defaults(func=lambda *args: solve(parser, *args))
+    parser.add_argument(
+        "--grammar", help="the grammar in BNF (if not passed as a file)"
+    )
+    parser.add_argument(
+        "--constraint", help="the ISLa constraint (if not passed as a file)"
+    )
+    parser.add_argument(
+        "--output-dir", help="a directory into which to place generated output files"
+    )
+    parser.add_argument(
+        "-n",
+        "--num-solutions",
+        type=int,
+        default=1,
+        help="the number of solutions to generate",
+    )
+    parser.add_argument(
+        "-t",
+        "--timeout",
+        type=float,
+        default=-1,
+        help="""
+The number of (fractions of) seconds after which the solver should stop finding
+solutions. Negative numbers imply that no timeout is set""",
+    )
+    parser.add_argument(
+        "--unsat-support",
+        type=bool,
+        action=argparse.BooleanOptionalAction,
+        default=False,
+        help="""
+Activate support for unsatisfiable constraints. This can be required to make the
+analysis of unsatisfiable constraints terminate, but reduces the performance of the
+generator for satisfiable formulas""",
+    )
+    parser.add_argument(
+        "-f",
+        "--free-instantiations",
+        type=int,
+        default=10,
+        help="""
+the number of times an unconstrained nonterminal should be randomly instantiated
+""",
+    )
+    parser.add_argument(
+        "-s",
+        "--smt-instantiations",
+        type=int,
+        default=10,
+        help="""
+the number of solutions obtained from the SMT solver for atomic SMT-LIB formulas""",
+    )
+    parser.add_argument(
+        "--unique-trees",
+        type=int,
+        default=False,
+        action=argparse.BooleanOptionalAction,
+        help="""
+Enforces the uniqueness of derivation trees in the solver queue. This setting can
+improve the generator performance, but can also lead to omitted interesting solutions
+in certain cases.""",
+    )
+    parser.add_argument(
+        "--unwinding-depth",
+        type=int,
+        default=4,
+        help="""
+Set the depth until which nonregular grammar elements in SMT-LIB expressions are
+unwound to make them regular before the SMT solver is queried""",
+    )
+    parser.add_argument(
+        "-w",
+        "--weight-vector",
+        help="""
+Set the ISLa weight vector. Expects a comma-separated list of floating point values""",
+        default="7,1,4,2,19",
+    )
+    parser.add_argument(
+        "-l",
+        "--log-level",
+        type=int,
+        default=1,
+        help="""
+Set the logging level; 0 only prints errors, 1 also warnings, 2 noncritical
+information, and 3 a debug trace""",
+    )
+
+    parser.add_argument(
+        "files",
+        nargs="*",
+        metavar="FILES",
+        type=argparse.FileType("r", encoding="UTF-8"),
+        help="""
+Possibly multiple ISLa constraint (`*.isla`) and BNF grammar (`*.bnf`) files. Multiple
+grammar files will be simply merged; multiple ISLa constraints will be combined to a
+disjunction. Note that you can _either_ pass a grammar as a file _or_ via the
+`--grammar` option; similarly for constraints and the `--constraint` option. Either
+way, a grammar and a constraint must be specified.""",
+    )
+
+
+def create_fuzz_parser(subparsers):
+    parser = subparsers.add_parser(
+        "fuzz",
+        help="pass solutions to an ISLa constraint to a test subject",
+        description="""
+Create solutions to an ISLa constraint and a reference grammar, and pass these to
+a test subject.""",
+    )
+    parser.set_defaults(func=lambda *args: fuzz(parser, *args))
+
+
+def create_check_parser(subparsers):
+    parser = subparsers.add_parser(
+        "check",
+        help="check whether an input satisfies an ISLa constraint",
+        description="""
+Check whether an input is derivable from a grammar and satisfies and an ISLa
+constraint.""",
+    )
+    parser.set_defaults(func=lambda *args: check(parser, *args))
+
+
+def create_parse_parser(subparsers):
+    parser = subparsers.add_parser(
+        "parse",
+        help="parse an input into a derivation tree if it satisfies an ISLa constraint",
+        description="""
+Parse an input into a derivation tree if it is derivable from a grammar and
+satisfies an ISLa constraint.""",
+    )
+    parser.set_defaults(func=lambda *args: parse(parser, *args))
 
 
 if __name__ == "__main__":
