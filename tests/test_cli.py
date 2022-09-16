c570f09fd0a79770171070ff350dbd457224be37
Dominic Steinhöfel <dominic.steinhoefel@cispa.de>
2022-09-16 10:16:27 +0200

    Temporarily focusing on tests that fail on GitHub only

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -573,6 +573,8 @@ exists <assgn> assgn:
             lines[bash_command_start : bash_command_start + bash_command_end]
         ).replace("\\", "")
 
+        print(bash_command)
+
         stdout, stderr, code = run_isla(*bash_command.split(" ")[1:])
         self.assertFalse(stderr)
         self.assertFalse(code)
