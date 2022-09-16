5e653274e42b6560b0188f8b8067bb5527e96f4f
Dominic Steinhöfel <dominic.steinhoefel@cispa.de>
2022-09-16 10:19:09 +0200

    -

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -573,6 +573,7 @@ exists <assgn> assgn:
             lines[bash_command_start : bash_command_start + bash_command_end]
         ).replace("\\", "")
 
+        print()
         print(bash_command)
 
         stdout, stderr, code = run_isla(*bash_command.split(" ")[1:])
